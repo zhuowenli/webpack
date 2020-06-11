bc8b488a718eb597b357e439a9de611db550bde2
zhuowenli <zhuowenligg@gmail.com>
2020-06-11 10:39:15 +0800

    :art: 兼容小程序

--- buildin/global.js
+++ buildin/global.js
@@ -5,12 +5,8 @@ g = (function() {
 	return this;
 })();
 
-try {
-	// This works if eval is allowed (see CSP)
-	g = g || new Function("return this")();
-} catch (e) {
-	// This works if the window reference is available
-	if (typeof window === "object") g = window;
+if (!g && typeof window === "object") {
+	g = window;
 }
 
 // g can still be undefined, but nothing to do about it...
